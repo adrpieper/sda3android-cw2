58e0a40e092c80d7aba81623e5bd10ba7d895e47
Adrian <adrpieper@gmail.com>
2017-02-25 12:00:53 +0100

    Pozostałe pola i objętości

--- src/Zad6.java
+++ src/Zad6.java
@@ -5,7 +5,11 @@ public class Zad6 {
 
         System.out.println("Pole kwadratu o boku 4 : " + poleKwadratu(4));
         System.out.println("Pole sześcianu o boku 2 : " + poleSzescianu(2));
-        System.out.println("Pole koła o boku 1 : " + poleKola(1));
+        System.out.println("Pole koła o promieniu 1 : " + poleKola(1));
+        System.out.println("Objętość walca o promieniu 1 i wysokości 10 : " + objetoscWalca(1,10));
+        System.out.println("Objętość stożka o promieniu 1 i wysokości 10 : " + objetoscStozka(1,10));
+        System.out.println("Objętość sześcianu o boku 2 : " + objetoscSzescianu(2));
+        System.out.println("Objętość ostrosłupa o boku 2 i wysokości 9 : " + objetoscOstroslupa(2,9));
     }
 
     private static double poleKwadratu(double a) {
@@ -18,5 +22,21 @@ public class Zad6 {
 
     private static double poleKola(double r) {
         return r*r*Math.PI;
+    }
+
+    private static double objetoscWalca(double r, double h) {
+        return r*r*Math.PI*h;
+    }
+
+    private static double objetoscStozka(double r, double h) {
+        return r*r*Math.PI*h/3;
+    }
+
+    private static double objetoscSzescianu(double a) {
+        return a*a*a;
+    }
+
+    private static double objetoscOstroslupa(double a, double h) {
+        return a*a*h/3;
     }
 }
