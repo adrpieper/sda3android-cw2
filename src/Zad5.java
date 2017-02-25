3d2ed3063b48e41141f526815cc33d0c538495e0
Adrian <adrpieper@gmail.com>
2017-02-25 09:19:56 +0100

    Wykonałem drugie zadanie

--- src/Zad5.java
+++ src/Zad5.java
@@ -3,12 +3,21 @@ public class Zad5 {
 
     public static void main(String[] args) {
 
-        suma(4,2);
+        int w = mnozenie(1,4);
+        System.out.println(w);
+
+        System.out.println(mnozenie(4,7));
+        suma(mnozenie(3,4),10);
     }
 
     private static void suma(int a,int b) {
 
         int wynik = a + b;
         System.out.println(wynik);
+    }
+
+    private static int mnozenie(int a,int b) {
+        int wynik = a * b;
+        return wynik;
     }
 }
