0acb5559435f17f3ef7a709ae75b4d8e1ad48589
Adrian <adrpieper@gmail.com>
2017-02-25 10:00:25 +0100

    Wykonałem trzecie zadanie

--- src/Zad5.java
+++ src/Zad5.java
@@ -8,16 +8,30 @@ public class Zad5 {
 
         System.out.println(mnozenie(4,7));
         suma(mnozenie(3,4),10);
+        int i = 10;
+        suma(i,5);
+        System.out.println(i);
     }
 
-    private static void suma(int a,int b) {
+    private static void suma(int c,int d) {
 
-        int wynik = a + b;
+        c = 100;
+        int wynik = c + d;
         System.out.println(wynik);
     }
 
     private static int mnozenie(int a,int b) {
         int wynik = a * b;
         return wynik;
+    }
+
+    private static int min(int[] tab) {
+        int m = Integer.MAX_VALUE;
+        for (int i = 0 ; i < tab.length; i++){
+            if (m > tab[i]) {
+                m = tab[i];
+            }
+        }
+        return m;
     }
 }
