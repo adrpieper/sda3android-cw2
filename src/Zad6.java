a99ce33cc1d6941e5401bbc725415410bed82cd5
Adrian <adrpieper@gmail.com>
2017-02-25 11:18:27 +0100

    Pole kwadratu

---
+++ src/Zad6.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Adrian on 2017-02-25.
+ */
+public class Zad6 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Pole kwadratu o boku 4 : " + poleKwadratu(4));
+    }
+
+    private static double poleKwadratu(double a) {
+        return a*a;
+    }
+}
