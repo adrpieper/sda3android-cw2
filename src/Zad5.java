7fc50cc51d05798fc82a4fcd69bf1d546bb40dd3
Adrian <adrpieper@gmail.com>
2017-02-25 09:10:33 +0100

    Wykonałem pierwsze zadanie

--- src/Zad5.java
+++ src/Zad5.java
@@ -3,5 +3,12 @@ public class Zad5 {
 
     public static void main(String[] args) {
 
+        suma(4,2);
+    }
+
+    private static void suma(int a,int b) {
+
+        int wynik = a + b;
+        System.out.println(wynik);
     }
 }
