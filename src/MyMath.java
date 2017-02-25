75e2d18f6562d20ed20eec37a9523502b9b182a4
Adrian <adrpieper@gmail.com>
2017-02-25 14:16:17 +0100

    Dodałem metodę max

---
+++ src/MyMath.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Adrian on 2017-02-25.
+ */
+public class MyMath {
+
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }else {
+            return b;
+        }
+    }
+
+    public static long max(long a, long b) {
+        if (a > b) {
+            return a;
+        }else {
+            return b;
+        }
+    }
+
+    public static float max(float a, float b) {
+        if (a > b) {
+            return a;
+        }else {
+            return b;
+        }
+    }
+
+    public static double max(double a, double  b) {
+        if (a > b) {
+            return a;
+        }else {
+            return b;
+        }
+    }
+}
