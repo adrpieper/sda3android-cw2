1eca09717a193b7dc96b3b6ce0cbff7c9779519b
Adrian <adrpieper@gmail.com>
2017-02-25 11:11:30 +0100

    Zliczanie znaków

--- src/Foreach.java
+++ src/Foreach.java
@@ -6,7 +6,7 @@ public class Foreach {
         int[] liczby = {1,4,11,4};
         System.out.println("Suma : " + sum(liczby));
         System.out.println("Iloczyn : " + iloczyn(new int[]{1,2,4}));
-
+        System.out.println("Znaki : " + znaki("hallo world", 'l'));
     }
 
     private static int sum(int[] tab) {
@@ -23,5 +23,16 @@ public class Foreach {
             s *= i;
         }
         return s;
+    }
+
+    private static int znaki(String text, char znak) {
+        char[] znaki = text.toCharArray();
+        int i = 0;
+        for (char z : znaki) {
+            if (z==znak) {
+                i++;
+            }
+        }
+        return i;
     }
 }
