91bdd2d8511f0d436e8514ecf7cee2b36ee7f4db
Adrian <adrpieper@gmail.com>
2017-02-25 15:19:14 +0100

    Utworzyłem klasę QuadraticEquation z trzema polami.

---
+++ src/equation/QuadraticEquation.java
@@ -0,0 +1,16 @@
+package equation;
+
+/**
+ * Created by Adrian on 2017-02-25.
+ */
+public class QuadraticEquation {
+    private double a;
+    private double b;
+    private double c;
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
