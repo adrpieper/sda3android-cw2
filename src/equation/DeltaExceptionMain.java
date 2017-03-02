a8c3cc1e5ad08338539c6f9b0d8b53fad4b2da0a
Adrian <adrpieper@gmail.com>
2017-03-02 20:05:55 +0100

    Dodałem klasę DeltaExceptionMain

---
+++ src/equation/DeltaExceptionMain.java
@@ -0,0 +1,20 @@
+package equation;
+
+import java.util.Scanner;
+
+/**
+ * Created by Adrian on 2017-03-02.
+ */
+public class DeltaExceptionMain {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj a");
+        double a = scanner.nextDouble();
+        System.out.println("Podaj b");
+        double b = scanner.nextDouble();
+        System.out.println("Podaj c");
+        double c = scanner.nextDouble();
+
+    }
+}
